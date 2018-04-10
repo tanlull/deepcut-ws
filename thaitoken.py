2f695274a82c790f8f76f1e23a5a1b42e8ff640c
tanlull <tanlull@gmail.com>
2018-04-10 16:48:11 +0700

    add proc pip

--- thaitoken.py
+++ thaitoken.py
@@ -37,5 +37,6 @@ def about():
 	
 if __name__ == "__main__":
     #app.debug = True
-	from os import environ
-	app.run(host='0.0.0.0', port=environ.get("PORT", 5000))
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
+	
