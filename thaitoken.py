2577e0f6ad5b8ddd0471e5b62b224bda17adcb2a
tanlull <tanlull@gmail.com>
2018-04-10 16:52:12 +0700

    CHANGE PORT

--- thaitoken.py
+++ thaitoken.py
@@ -35,8 +35,8 @@ def about():
 		PullDeepcutFrom="https://github.com/rkcosmos/deepcut"
     )
 	
-if __name__ == "__main__":
-    #app.debug = True
-	port = int(os.environ.get('PORT', 5000))
+if __name__ == "__main__":    
+	from os import environ
+	port = int(environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
 	
